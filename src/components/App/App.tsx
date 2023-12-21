fc378b1adbaf6aae80ea0f53b68330293a3ae5ff
Barbara-Cerovsek <barbara.cerovsek@oclock.school>
2023-12-21 17:39:21 +0100

    challenge weather app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Icon } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [zip, setZip] = useState(64500);
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fahrenheitToCelsius = (fahren) => {
+    return Math.round(fahren - 273.15);
+  };
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=44b69afe1e2a24d74186cc777a88f9a2`
+    );
+
+    if (response.data) {
+      response.data.main.temp = fahrenheitToCelsius(response.data.main.temp);
+    }
+    setWeatherData(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setZip(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="widget">
+      <Icon name="sun" size="huge" />
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city zipcode"
+          value={zip}
+          onChange={handleInputChange}
+        />
+      </form>
+      {weatherData ? (
+        <>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+          <img
+            src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+            alt={weatherData.weather[0].description}
+          />
+        </>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
